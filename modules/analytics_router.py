32d11fe414c2adcb5f7744fdf65905148f11788e
prithvi27-max <prithviawate27@gmail.com>
2026-01-31 14:07:41 +0530

    Your message

--- modules/analytics_router.py
+++ modules/analytics_router.py
@@ -113,7 +113,7 @@ def process_query(query: str, language: str = "en"):
             return (
                 pred_df
                 .sort_values("Attrition_Risk", ascending=False)
-                .head(20)
+                .head()
             )
         except Exception:
             return "⚠ Unable to run attrition prediction model."
